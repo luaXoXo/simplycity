bae307ad82e8ee4d23714e6d431e823f586e3bc5
luaXoXo <79286488+luaXoXo@users.noreply.github.com>
2024-11-06 14:45:02 -0300

    js

---
+++ js/menu.js
@@ -0,0 +1,7 @@
+const botaoMenu = document.querySelector (".titulo-menu a");
+
+const listaDeLinks = document.querySelector(".links-menu");
+
+botaoMenu.addEventListener("click", function(){
+    console.log("opaaaa");
+});
